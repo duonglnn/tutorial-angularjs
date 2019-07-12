5adb1e4e9013466ef4dae8c4669aeafa6589492d
johnny <johnny@travala.com>
2019-07-12 14:26:48 +0700

    Adding ui-router

---
+++ route/ui-router/app.js
@@ -0,0 +1,25 @@
+var routerApp = angular.module('routerApp', ['ui.router']);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home'); //Mọi đường dẫn không hợp lệ đều được chuyển đến state home
+
+    $stateProvider
+
+        .state('product', { // Định ngĩa 1 state product
+            url: '/product', // khai báo Url hiển thị
+            templateUrl: 'templates/product.html', //đường dẫn view
+            controller: 'PrtController' //// Khai báo 1 controller cho state product
+        })
+
+        .state('home', {
+            url: '/home',
+            templateUrl: 'templates/home.html'
+        })
+
+        .state('about', {
+            url: '/about',
+            templateUrl: 'templates/about.html'
+        })
+
+});
